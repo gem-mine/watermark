87fa4074821aa583db0b0a74a0237b31ac0048f8
GYL <sehun@0412.com>
2020-12-01 18:37:19 +0800

    init by gem-mine 👻

---
+++ src/demo/es6.ts
@@ -0,0 +1,27 @@
+const p = new Promise((resolve) => {
+  console.log('In Promise')
+  setTimeout(() => {
+    resolve()
+  }, 100)
+})
+
+p.then(() => {
+  console.log('resolved')
+})
+
+export default class Home {
+  name = ''
+  constructor(name) {
+    this.name = name || 'home'
+  }
+
+  log() {
+    for (const letter of this.name) {
+      console.log(letter)
+    }
+  }
+}
+
+const myHome = new Home('myHome')
+
+myHome.log()
